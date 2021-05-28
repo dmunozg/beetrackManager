ae499caf5b7afee02c3efbcde1c3d9fe27b8210a
Diego Muñoz <diego.munoz.g@ug.uchile.cl>
2021-05-28 01:28:41 -0400

    primera versión funcional

--- app.py
+++ app.py
@@ -1,16 +1,139 @@
 from beetrack.beetrack_api import BeetrackAPI
 from beetrack.beetrack_objects import Item, Dispatch
+from beetrack import xls_import, mail_handler
+from time import sleep
+from dotenv import load_dotenv
+import json, os, time, sys
+import pandas as pd
 
 BASE_URL = "https://app.beetrack.com/api/external/v1"
+ALLOWED_FILES_EXTENSIONS = [
+    "xlsx",
+]
+# TODO
+# Esto hay que reemplazarlo por una base de datos SQL
+ALLOWED_CLIENTS_DF = pd.DataFrame(
+    [
+        ["lkeeler.flipout@gmail.com", "Pruebas", "TEST", "Reibo 3619, Puente Alto"],
+        [
+            "matias@logicaexpress.cl",
+            "Pruebas",
+            "TEST",
+            "Cerro Loma Larga 3610, Puente Alto",
+        ],
+    ],
+    columns=["allowedEmail", "clientName", "codePrefix", "pickupAddress"],
+)
 
-# Import API KEY from file. This file should not be public
-with open("APIKEY", "r") as APIKeyfile:
-    API_KEY = APIKeyfile.read()
 
-LogicaAPI = BeetrackAPI(API_KEY, BASE_URL)
+def check_if_allowed(filename):
+    extension = filename.split(".")[-1]
+    if extension in ALLOWED_FILES_EXTENSIONS:
+        return True
+    else:
+        return False
 
-testDispatch = LogicaAPI.get_dispatch("1004")
-testDispatchObject = Dispatch(testDispatch["response"])
 
-print(testDispatchObject.dump_dict())
-print(testDispatchObject.dump_json())
+load_dotenv()
+LogicaAPI = BeetrackAPI(os.getenv("BEETRACK_APIKEY"), BASE_URL)
+
+MailInbox = mail_handler.Inbox(
+    os.getenv("IMAP_USER"), os.getenv("IMAP_PASSWD"), os.getenv("IMAP_SERVER")
+)
+
+fetchedEmails = MailInbox.check_inbox()
+if len(fetchedEmails) == 0:
+    timestamp = time.strftime("%H:%M:%S")
+    print(f"[{timestamp}] No new mail found. Exiting.", file=sys.stdout)
+
+for email in fetchedEmails:
+    if email._from in ALLOWED_CLIENTS_DF["allowedEmail"].to_list():
+        timestamp = time.strftime("%H:%M:%S")
+        print(
+            f"[{timestamp}] MailID {email.id} found from allowed client: {email._from}"
+        )
+    else:
+        continue
+    if any([check_if_allowed(att) for att in email.attachments]):
+        print(f"MailID {email.id} contains a valid file", file=sys.stdout)
+    else:
+        print(f"MailID {email.id} doesn't contain any valid file. Skipping.")
+        email.mark_read()
+        continue
+    clientName = (
+        ALLOWED_CLIENTS_DF["clientName"]
+        .where(ALLOWED_CLIENTS_DF["allowedEmail"] == email._from)
+        .dropna()[0]
+    )
+    pickupAddress = (
+        ALLOWED_CLIENTS_DF["pickupAddress"]
+        .where(ALLOWED_CLIENTS_DF["allowedEmail"] == email._from)
+        .dropna()[0]
+    )
+    timestamp = time.strftime("%H:%M:%S")
+    for attachment in email.attachments:
+        if not check_if_allowed(attachment):
+            continue
+        else:
+            print(
+                f"[{timestamp}] Will scan file for dispatches for client {clientName} to be picked up from {pickupAddress}",
+                file=sys.stdout,
+            )
+        foundDispatchesData, warnings = xls_import.xlsx_to_dispatches(
+            attachment, clientName, pickupAddress
+        )
+        if len(foundDispatchesData) == 0:
+            print(
+                "[{timestamp}] Could not parse any dispatches in file {filename}:".format(
+                    timestamp=time.strftime("%H:%M:%S"),
+                    filename=os.path.basename(attachment),
+                ),
+                file=sys.stderr,
+            )
+            print(warnings, sep="\n", file=sys.stderr)
+        elif len(warnings) > 0:
+            print(
+                "[{timestamp}] There were errors importing dispatches from file {filename}:".format(
+                    timestamp=time.strftime("%H:%M:%S"),
+                    filename=os.path.basename(attachment),
+                ),
+                file=sys.stderr,
+            )
+            print(warnings, sep="\n", file=sys.stderr)
+        else:
+            pass
+        for newDispatch, errorCode, warnings in foundDispatchesData:
+            if errorCode == 2:
+                print(
+                    "[{timestamp}] Failed to import dispatch {dispatchCode}:".format(
+                        timestamp=time.strftime("%H:%M:%S"),
+                        dispatchCode=newDispatch.id,
+                    ),
+                    file=sys.stderr,
+                )
+                print(*warnings, sep="\n", file=sys.stderr)
+                continue
+            else:
+                response = LogicaAPI.create_dispatch(newDispatch.dump_dict())
+                print(response)
+            if errorCode == 1:
+                print(
+                    "[{timestamp}] Dispatch {dispatchCode} was imported with issues:".format(
+                        timestamp=time.strftime("%H:%M:%S"),
+                        dispatchCode=newDispatch.id,
+                    ),
+                    file=sys.stderr,
+                )
+                print(*warnings, sep="\n", file=sys.stderr)
+            elif errorCode == 0:
+                print(
+                    "[{timestamp}] Dispatch {dispatchCode} was imported successfully".format(
+                        timestamp=time.strftime("%H:%M:%S"),
+                        dispatchCode=newDispatch.id,
+                    ),
+                    file=sys.stdout,
+                )
+            else:
+                print("CRITICAL: Unknown error code.")
+    email.mark_read()
+print("[{timestamp}] Checked all mails. Going to sleep.")
---
+++ beetrack/mail_handler.py
@@ -0,0 +1,153 @@
+import imaplib, email, os, re, time, sys
+from pathlib import Path
+from copy import copy
+
+
+def clean(text):
+    # clean text for creating a folder
+    return "".join(c if c.isalnum() else "_" for c in text)
+
+
+class Email:
+    def __init__(
+        self,
+        id,
+        subject,
+        _from,
+        recipient,
+        body=None,
+        seen=False,
+        attachments=[],
+        fromInbox=None,
+    ):
+        self.subject = subject
+        self._from = _from
+        self.recipient = recipient
+        self.body = body
+        self.attachments = attachments
+        self.id = id
+        self.Inbox = fromInbox
+        self.seen = seen
+
+    def mark_read(self):
+        timestamp = time.strftime("%H:%M:%S")
+        if self.seen:
+            print(f"[{timestamp}] INFO: mail {self.id} is already marked as read")
+        self.Inbox.reconnect()
+        self.Inbox.imap.select("INBOX")
+        self.Inbox.imap.store(self.id, "+FLAGS", "\SEEN")
+        print(f"[{timestamp}] MailID {self.id} from {self._from} marked as read")
+        self.seen = True
+
+
+class Inbox:
+    def __init__(self, user, password, imapServer):
+        self.user = user
+        self.password = password
+        self.imapServer = imapServer
+
+        self.imap = imaplib.IMAP4_SSL(imapServer)
+        try:
+            loginResponse = self.imap.login(user, password)
+        except:
+            raise Exception(
+                "Critical: Unhandled error when trying to login to IMAP server."
+            )
+
+    def reconnect(self):
+        self.imap = imaplib.IMAP4_SSL(self.imapServer)
+        self.imap.login(self.user, self.password)
+
+    def check_inbox(
+        self, folder="INBOX", unread_only=True, last=None, attachmentsDir="attachments"
+    ):
+        response, nEmails = self.imap.select(folder)
+        nEmails = int(nEmails[0])
+        if unread_only:
+            response, unreadEmailIDs = self.imap.search(None, "UNSEEN")
+            emailRange = unreadEmailIDs[0].decode("utf-8").split()
+        else:
+            if last:
+                lastEmailToCheck = nEmails - last
+            else:
+                lastEmailToCheck = 1
+            emailRange = range(nEmails, lastEmailToCheck, -1)
+
+        def _fetch_flags(mailID):
+            rawFlags = self.imap.fetch(mailID, "FLAGS")
+            decodedFlags = rawFlags[1][0].decode("utf-8")
+            flagsPattern = re.compile(r"[\d]+\s\(FLAGS\s\(([\\\s\w]+)\)\)")
+            if flagsPattern.match(decodedFlags):
+                return flagsPattern.match(decodedFlags)[1].split()
+            else:
+                return []
+
+        foundEmails = []
+        for mailID in emailRange:
+            # Leer cabecera del correo
+            response, rawEmail = self.imap.fetch(str(mailID), "(RFC822)")
+            for content in rawEmail:
+                if isinstance(content, tuple):
+                    rawEmail = content
+            parsedEmail = email.message_from_bytes(rawEmail[1])
+            rawSubject, encoding = email.header.decode_header(
+                parsedEmail.get("Subject")
+            )[0]
+            if isinstance(rawEmail, bytes):
+                subject = rawSubject.decode(encoding)
+            else:
+                subject = rawSubject
+            _from = email.utils.parseaddr(parsedEmail["FROM"])[1]
+            rawRecipients = email.header.decode_header(parsedEmail.get("To"))[0]
+            if isinstance(rawRecipients, bytes):
+                recipients = rawRecipients.decode(encoding)
+            else:
+                recipients = rawRecipients
+            recipientList = [rec.strip() for rec in recipients[0].split(",")]
+            # Crear objeto Email
+            currentEmail = Email(
+                mailID, subject, _from, recipientList, fromInbox=self, attachments=[]
+            )
+            if "\\Seen" in _fetch_flags(mailID):
+                currentEmail.seen = True
+            # Leer contenido del correo
+            # if the email message is multipart
+            if parsedEmail.is_multipart():
+                # iterate over email parts
+                for part in parsedEmail.walk():
+                    # extract content type of email
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get("Content-Disposition"))
+                    try:
+                        # get the email body
+                        body = part.get_payload(decode=True).decode()
+                    except:
+                        pass
+                    if "attachment" in content_disposition:
+                        # download attachment
+                        filename = part.get_filename()
+                        if filename:
+                            year_month = time.strftime("%Y-%m")
+                            day = time.strftime("%d-%a")
+                            folder_name = os.path.join(
+                                attachmentsDir, year_month, clean(_from), day, mailID
+                            )
+                            Path(folder_name).mkdir(parents=True, exist_ok=True)
+                            filepath = os.path.join(folder_name, filename)
+                            currentEmail.attachments.append(filepath)
+                            # download attachment and save it
+                            open(filepath, "wb").write(part.get_payload(decode=True))
+            else:
+                # extract content type of email
+                content_type = parsedEmail.get_content_type()
+                # get the email body
+                body = parsedEmail.get_payload(decode=True).decode()
+            if body:
+                currentEmail.body = body
+            foundEmails.append(copy(currentEmail))
+            timestamp = time.strftime("%H:%M:%S")
+            print(
+                f'[{timestamp}] Fetching email from {_from} subject: "{subject}" id: {mailID}',
+                file=sys.stdout,
+            )
+        return foundEmails
