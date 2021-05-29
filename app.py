ae46a8579bd58f4888dd8b5e22cdd7933956b4a7
Diego Muñoz <diego.munoz.g@ug.uchile.cl>
2021-05-29 19:09:49 -0400

    Ahora revisa si todas las variables de entorno están

--- app.py
+++ app.py
@@ -2,9 +2,43 @@ from beetrack.beetrack_api import BeetrackAPI
 from beetrack.beetrack_objects import Item, Dispatch
 from beetrack import xls_import, mail_handler
 from time import sleep
-from dotenv import load_dotenv
-import json, os, time, sys
+import os, time, sys
 import pandas as pd
+
+REQUIRED_ENV_VARIABLES = ["BEETRACK_APIKEY", "IMAP_USER", "IMAP_PASSWD", "IMAP_SERVER"]
+
+
+def check_if_env_is_set(env):
+    if os.getenv(env):
+        return True
+    else:
+        return False
+
+
+if not all([check_if_env_is_set(env) for env in REQUIRED_ENV_VARIABLES]):
+    print(
+        "[{timestamp}] Not all required environment variables were set before runtime. Loading from .env file.".format(
+            timestamp=time.strftime("%H:%M:%S")
+        )
+    )
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+
+def check_env_definitive(env):
+    if not os.getenv(env):
+        print(
+            "[{timestamp}] Critical: Environment variable {env} was not set.".format(
+                timestamp=time.strftime("%H:%M:%S"), env=env
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+
+for env in REQUIRED_ENV_VARIABLES:
+    check_env_definitive(env)
 
 BASE_URL = "https://app.beetrack.com/api/external/v1"
 ALLOWED_FILES_EXTENSIONS = [
@@ -34,114 +68,119 @@ def check_if_allowed(filename):
         return False
 
 
-load_dotenv()
-LogicaAPI = BeetrackAPI(os.getenv("BEETRACK_APIKEY"), BASE_URL)
+def main():
+    LogicaAPI = BeetrackAPI(os.getenv("BEETRACK_APIKEY"), BASE_URL)
 
-MailInbox = mail_handler.Inbox(
-    os.getenv("IMAP_USER"), os.getenv("IMAP_PASSWD"), os.getenv("IMAP_SERVER")
-)
+    MailInbox = mail_handler.Inbox(
+        os.getenv("IMAP_USER"), os.getenv("IMAP_PASSWD"), os.getenv("IMAP_SERVER")
+    )
 
-fetchedEmails = MailInbox.check_inbox()
-if len(fetchedEmails) == 0:
-    timestamp = time.strftime("%H:%M:%S")
-    print(f"[{timestamp}] No new mail found. Exiting.", file=sys.stdout)
-    MailInbox.logout()
-    sys.exit(0)
-for email in fetchedEmails:
-    if email._from in ALLOWED_CLIENTS_DF["allowedEmail"].to_list():
+    fetchedEmails = MailInbox.check_inbox()
+    if len(fetchedEmails) == 0:
         timestamp = time.strftime("%H:%M:%S")
-        print(
-            f"[{timestamp}] MailID {email.id} found from allowed client: {email._from}"
-        )
-    else:
-        continue
-    if any([check_if_allowed(att) for att in email.attachments]):
-        print(f"MailID {email.id} contains a valid file", file=sys.stdout)
-    else:
-        print(f"MailID {email.id} doesn't contain any valid file. Skipping.")
-        email.mark_read()
-        continue
-    clientName = (
-        ALLOWED_CLIENTS_DF["clientName"]
-        .where(ALLOWED_CLIENTS_DF["allowedEmail"] == email._from)
-        .dropna()
-        .iloc[0]
-    )
-    pickupAddress = (
-        ALLOWED_CLIENTS_DF["pickupAddress"]
-        .where(ALLOWED_CLIENTS_DF["allowedEmail"] == email._from)
-        .dropna()
-        .iloc[0]
-    )
-    timestamp = time.strftime("%H:%M:%S")
-    for attachment in email.attachments:
-        if not check_if_allowed(attachment):
+        print(f"[{timestamp}] No new mail found. Exiting.", file=sys.stdout)
+        MailInbox.logout()
+        return 0
+    for email in fetchedEmails:
+        if email._from in ALLOWED_CLIENTS_DF["allowedEmail"].to_list():
+            timestamp = time.strftime("%H:%M:%S")
+            print(
+                f"[{timestamp}] MailID {email.id} found from allowed client: {email._from}"
+            )
+        else:
             continue
+        if any([check_if_allowed(att) for att in email.attachments]):
+            print(f"MailID {email.id} contains a valid file", file=sys.stdout)
         else:
-            print(
-                f"[{timestamp}] Will scan file for dispatches for client {clientName} to be picked up from {pickupAddress}",
-                file=sys.stdout,
-            )
-        foundDispatchesData, warnings = xls_import.xlsx_to_dispatches(
-            attachment, clientName, pickupAddress
+            print(f"MailID {email.id} doesn't contain any valid file. Skipping.")
+            email.mark_read()
+            continue
+        clientName = (
+            ALLOWED_CLIENTS_DF["clientName"]
+            .where(ALLOWED_CLIENTS_DF["allowedEmail"] == email._from)
+            .dropna()
+            .iloc[0]
         )
-        if len(foundDispatchesData) == 0:
-            print(
-                "[{timestamp}] Could not parse any dispatches in file {filename}:".format(
-                    timestamp=time.strftime("%H:%M:%S"),
-                    filename=os.path.basename(attachment),
-                ),
-                file=sys.stderr,
-            )
-            print(warnings, sep="\n", file=sys.stderr)
-        elif len(warnings) > 0:
-            print(
-                "[{timestamp}] There were errors importing dispatches from file {filename}:".format(
-                    timestamp=time.strftime("%H:%M:%S"),
-                    filename=os.path.basename(attachment),
-                ),
-                file=sys.stderr,
-            )
-            print(warnings, sep="\n", file=sys.stderr)
-        else:
-            pass
-        for newDispatch, errorCode, warnings in foundDispatchesData:
-            if errorCode == 2:
-                print(
-                    "[{timestamp}] Failed to import dispatch {dispatchCode}:".format(
-                        timestamp=time.strftime("%H:%M:%S"),
-                        dispatchCode=newDispatch.id,
-                    ),
-                    file=sys.stderr,
-                )
-                print(*warnings, sep="\n", file=sys.stderr)
+        pickupAddress = (
+            ALLOWED_CLIENTS_DF["pickupAddress"]
+            .where(ALLOWED_CLIENTS_DF["allowedEmail"] == email._from)
+            .dropna()
+            .iloc[0]
+        )
+        timestamp = time.strftime("%H:%M:%S")
+        for attachment in email.attachments:
+            if not check_if_allowed(attachment):
                 continue
             else:
-                response = LogicaAPI.create_dispatch(newDispatch.dump_dict())
-                print(response)
-            if errorCode == 1:
                 print(
-                    "[{timestamp}] Dispatch {dispatchCode} was imported with issues:".format(
+                    f"[{timestamp}] Will scan file for dispatches for client {clientName} to be picked up from {pickupAddress}",
+                    file=sys.stdout,
+                )
+            foundDispatchesData, warnings = xls_import.xlsx_to_dispatches(
+                attachment, clientName, pickupAddress
+            )
+            if len(foundDispatchesData) == 0:
+                print(
+                    "[{timestamp}] Could not parse any dispatches in file {filename}:".format(
                         timestamp=time.strftime("%H:%M:%S"),
-                        dispatchCode=newDispatch.id,
+                        filename=os.path.basename(attachment),
                     ),
                     file=sys.stderr,
                 )
-                print(*warnings, sep="\n", file=sys.stderr)
-            elif errorCode == 0:
+                print(warnings, sep="\n", file=sys.stderr)
+            elif len(warnings) > 0:
                 print(
-                    "[{timestamp}] Dispatch {dispatchCode} was imported successfully".format(
+                    "[{timestamp}] There were errors importing dispatches from file {filename}:".format(
                         timestamp=time.strftime("%H:%M:%S"),
-                        dispatchCode=newDispatch.id,
+                        filename=os.path.basename(attachment),
                     ),
-                    file=sys.stdout,
+                    file=sys.stderr,
                 )
+                print(warnings, sep="\n", file=sys.stderr)
             else:
-                print("CRITICAL: Unknown error code.")
-    email.mark_read()
-MailInbox.logout()
-print(
-    "[{timestamp}] Checked all mails. Going to sleep.".format(
-        timestamp=time.strftime("%H:%M:%S")
+                pass
+            for newDispatch, errorCode, warnings in foundDispatchesData:
+                if errorCode == 2:
+                    print(
+                        "[{timestamp}] Failed to import dispatch {dispatchCode}:".format(
+                            timestamp=time.strftime("%H:%M:%S"),
+                            dispatchCode=newDispatch.id,
+                        ),
+                        file=sys.stderr,
+                    )
+                    print(*warnings, sep="\n", file=sys.stderr)
+                    continue
+                else:
+                    response = LogicaAPI.create_dispatch(newDispatch.dump_dict())
+                    print(response)
+                if errorCode == 1:
+                    print(
+                        "[{timestamp}] Dispatch {dispatchCode} was imported with issues:".format(
+                            timestamp=time.strftime("%H:%M:%S"),
+                            dispatchCode=newDispatch.id,
+                        ),
+                        file=sys.stderr,
+                    )
+                    print(*warnings, sep="\n", file=sys.stderr)
+                elif errorCode == 0:
+                    print(
+                        "[{timestamp}] Dispatch {dispatchCode} was imported successfully".format(
+                            timestamp=time.strftime("%H:%M:%S"),
+                            dispatchCode=newDispatch.id,
+                        ),
+                        file=sys.stdout,
+                    )
+                else:
+                    print("CRITICAL: Unknown error code.")
+        email.mark_read()
+    MailInbox.logout()
+    print(
+        "[{timestamp}] Checked all mails. Going to sleep.".format(
+            timestamp=time.strftime("%H:%M:%S")
+        )
     )
-)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
